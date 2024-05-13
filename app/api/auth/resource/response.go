3352425f1caa1dfeff63e0dee45449991a5c1f42
SH2G <suhy427@gmail.com>
2024-05-14 02:10:17 +0900

    (feat): 토큰발급 resource 추가

--- app/api/auth/resource/response.go
+++ app/api/auth/resource/response.go
@@ -2,6 +2,11 @@ package resource
 
 type CreateTokenResponse struct {
 	AccessToken  string `json:"access_token"`
-	Expired      int64    `json:"expired"`
+	Expired      int64  `json:"expired"`
 	RefreshToken string `json:"refresh_token"`
+}
+
+type UpdateTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	Expired     int64  `json:"expired"`
 }
