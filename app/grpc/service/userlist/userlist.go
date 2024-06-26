4ef31845d09f5e9bbd06a7b8adfb2d1960c5b77e
WGCAT <seed9878@naver.com>
2024-06-27 01:38:43 +0900

    (chore): 유저리스트 조회 빌드 수정

--- app/grpc/service/userlist/userlist.go
+++ app/grpc/service/userlist/userlist.go
@@ -31,13 +31,14 @@ func (s *server) GetUserList(ctx context.Context, in *userlist.UserListRequest) 
 
 	// 3. 유저코드를 담기 위해 성취도 단계 함수 호출
 	code := common.PercentCal(uint64(per))
+	visualInfo, err := userlistRepository.FindVisualCode(code)
 
 	return &userlist.UserListResponse{
 		UserId:     uint64(res.ID),
 		Email:      res.Email,
 		Nickname:   res.Nickname,
 		Profile:    res.Profile,
-		VisualCode: code,
+		VisualCode: visualInfo.Code,
 		Created:    res.CreatedAt.Format(time.RFC3339),
 	}, nil
 }
